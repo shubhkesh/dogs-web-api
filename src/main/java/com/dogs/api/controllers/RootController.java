e3c17ca376682d2ebcb236759173f5cd66841cfc
shubhkesh <shubhkesh@users.noreply.github.com>
2026-04-19 12:55:03 +0530

    fix

---
+++ src/main/java/com/dogs/api/controllers/RootController.java
@@ -0,0 +1,13 @@
+package com.dogs.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/swagger-ui.html";
+    }
+}
