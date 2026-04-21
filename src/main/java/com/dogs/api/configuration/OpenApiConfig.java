2d3c56843980d52cb4c251398900213c7ff0cb67
shubhkesh <shubhkesh@users.noreply.github.com>
2026-04-21 22:30:58 +0530

    refactor code

--- src/main/java/com/dogs/api/configuration/OpenApiConfig.java
+++ src/main/java/com/dogs/api/configuration/OpenApiConfig.java
@@ -6,9 +6,16 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
 
     @Bean
     public OpenAPI dogsWebApiOpenAPI() {
--- src/main/java/com/dogs/api/constants/ApiConstants.java
+++ src/main/java/com/dogs/api/constants/ApiConstants.java
@@ -4,9 +4,6 @@ public class ApiConstants {
 
     public static final String API_V1 = "/api/v1";
     public static final String BREEDS_PATH = API_V1 + "/breeds";
-    public static final String BREED_BY_NAME_PATH = BREEDS_PATH + "/{breedName}";
-    public static final String SUB_BREEDS_PATH = BREED_BY_NAME_PATH + "/sub-breeds";
-    public static final String SUB_BREED_BY_NAME_PATH = SUB_BREEDS_PATH + "/{subBreedName}";
 
     private ApiConstants() {
         throw new IllegalStateException("Utility class");
